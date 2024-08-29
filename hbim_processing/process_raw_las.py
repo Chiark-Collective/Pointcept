6850f7d9961964f2299d7cf80b68b1b92199c27a
Liam Moore <liam@chiark.io>
2024-08-29 15:37:46 +0200

    add configurable data dir to hbim processing script

--- hbim_processing/process_raw_las.py
+++ hbim_processing/process_raw_las.py
@@ -1,20 +1,3 @@
-"""
-This script will take a category of hbim data ("park_row", "rog_north" etc) and 
-do the following:
-
-1. reads the finely sampled cloud .las files for each category present in the hbim set.
-2. applies a scaling operation to all axes to convert from cm to m
-3. subsamples the clouds according to the specified voxelisation
-4. switches the Z and Y axes so Z is vertical, as expected by Pointcept
-5. adds a new uniform scalar field to each input cloud called "gt", for the ground truth
-   category.
-6. merges the results
-7. uses the chipper filter to create a set of large voxels across the file to partition
-   the data into training and test samples
-8. saves the resultant files and moves them to the appropriate directories
-
-Needs to be run from the HBIM data root.
-"""
 import argparse
 import pdal
 import json
@@ -31,12 +14,15 @@ import numpy as np
 from pathlib import Path
 from collections import namedtuple
 
-
 # Constants
 VALID_LABELS = [
     'park_row',
+    'maritime_museum',
     'rog_north',
     'rog_south',
+    'library',
+    'brass_foundry',
+    'site',
 ]
 EXTRA_DIMS = "NormalX=float64, NormalY=float64, NormalZ=float64"
 CATEGORIES = [
@@ -55,72 +41,52 @@ CATEGORIES = [
     '13_other',
 ]
 
-
-###############################################################################
-# .las to .pth conversion
-###############################################################################
 def convert_all_las_to_pth(config):
     split_files = glob.glob(config.split_template)
     for f in split_files:
         las_to_pth(f)
 
-
 def las_to_pth(in_f, num_points=None):
-    """This function takes an input .las file and outputs a PyTorch state dictionary that is compatible with
-    Pointcept's data config for HBIM data.
-    """
     in_f = Path(in_f)
-
     output_dir = in_f.parent
 
     with laspy.open(str(in_f)) as file:
         las = file.read()
 
-        # Determine the number of points to process
         total_points = len(las.points)
         if num_points is None:
-            num_points = total_points  # Use all points if num_points is not given
+            num_points = total_points
         max_points = min(num_points, total_points)
 
-        # Set the output filename
         output_pth_path = in_f.with_suffix('.pth')
         output_pth_path = output_pth_path.parent / 'pth' / output_pth_path.name
         if num_points != total_points:
             output_pth_path = output_pth_path.with_stem(f'{output_pth_path.stem}_n{num_points}')
-        
-        # Read and adjust coordinates to start at zero
+
         x_adjusted = las.x[:max_points] - np.min(las.x[:max_points])
         y_adjusted = las.y[:max_points] - np.min(las.y[:max_points])
         z_adjusted = las.z[:max_points] - np.min(las.z[:max_points])
-        
-        # Create the coordinate array
+
         coord = np.stack((x_adjusted, y_adjusted, z_adjusted), axis=-1).astype(np.float32)
-        
-        # Check if RGB data exists
+
         if hasattr(las, 'red') and hasattr(las, 'green') and hasattr(las, 'blue'):
             color = np.stack((las.red[:max_points], las.green[:max_points], las.blue[:max_points]), axis=-1).astype(np.float32)
-            color /= 256.0  # Normalize color
+            color /= 256.0
         else:
-            raise ValueError(
-                "Error: input cloud does not contain RGB info. This is likely a .las version or \
-                point format mismatch. Check how your .las clouds were saved."
-                )
+            raise ValueError("Error: input cloud does not contain RGB info.")
 
-        # Extract normals if they exist
         if hasattr(las, 'NormalX') and hasattr(las, 'NormalY') and hasattr(las, 'NormalZ'):
             normal = np.stack((las.NormalX[:num_points], las.NormalY[:num_points], las.NormalZ[:num_points]), axis=-1)
             norm = np.linalg.norm(normal, axis=1, keepdims=True)
-            normal = (normal / norm).astype(np.float32)  # Normalize normals
+            normal = (normal / norm).astype(np.float32)
         else:
             raise ValueError("Error: missing normals. Check your .las format.")
 
-        # Extract gt if it exists
         if hasattr(las, 'gt'):
             gt = las.gt[:num_points].astype(np.int64)
         else:
             raise ValueError("Error: input cloud lacks ground truth information.")
 
-        # Save numpy arrays into a dictionary and then to a .pth file
         data = {
             'coord': coord,
             'color': color,
@@ -128,87 +94,69 @@ def las_to_pth(in_f, num_points=None):
             'normal': normal,
             'gt': gt,
         }
-        
-        # Save the dictionary as a .pth file
+
         torch.save(data, output_pth_path)
         print(f"- saved {max_points} points to {output_pth_path}")
     return
 
-
-###############################################################################
-# PDAL pipelines
-###############################################################################
-def run_pdal_single_category(label, category, resolution):
+def run_pdal_single_category(config, category):
     gt_index = int(category.split('_')[0])
     pipeline_json = {
         "pipeline": [
-            # Read the .las
             {
                 "type": "readers.las",
-                "filename": f"raw_clouds/{label}/{category}.las",
+                "filename": str(config.raw_dir / f"{category}.las"),
                 "extra_dims": EXTRA_DIMS
             },
-            # Subsample at requested resolution. Cloud is still in cm so we mult by 100.
             {
                 "type": "filters.voxelcenternearestneighbor",
-                "cell": resolution * 100
+                "cell": config.resolution * 100
             },
-            # Scale down
             {
                 "type":"filters.transformation",
                 "matrix":"0.01  0  0  0  0  0.01  0  0  0  0  0.01  0  0  0  0  1"
             },
-            # Switch Z and Y axes
             {
                 "type": "filters.transformation",
                 "matrix": "1 0 0 0 0 0 -1 0 0 1 0 0 0 0 0 1"
             },
-            # Add new scalar field called gt
             {
                 "type": "filters.ferry",
                 "dimensions": "=>gt"
             },
-            # Uniformly assign the ground truth category to gt
             {
                 "type": "filters.assign",
                 "assignment": f"gt[:]={gt_index}"
             },
-            # Write the output
             {
                 "type": "writers.las",
-                "filename": f"processed_clouds/resolution_{resolution}/{label}/{category}.las",
+                "filename": str(config.base_dir / f"{category}.las"),
                 "forward": "all",
                 "extra_dims": "all",
                 "minor_version": 4,
                 "dataformat_id": 8
             }
         ]
-        
     }
     pipeline = pdal.Pipeline(json.dumps(pipeline_json))
     print(f" - processing category: {category}")
     count = pipeline.execute()
     print(f"   - total points processed: {count}")   
 
-
-def run_all_categories(label, resolution):
+def run_all_categories(config):
     for category in CATEGORIES:
-        # First check the category file exists, some of the sets are nonexhaustive
-        fname = f"raw_clouds/{label}/{category}.las"
-        if not Path(fname).exists():
+        fname = config.raw_dir / f"{category}.las"
+        if not fname.exists():
             continue
-        run_pdal_single_category(label, category, resolution)
-
+        run_pdal_single_category(config, category)
 
 def run_merger_pipeline(config):
-    label = config.label
-    resolution = config.resolution
     input_files = []
     for category in CATEGORIES:       
-        fname = f"processed_clouds/resolution_{resolution}/{label}/{category}.las"
-        if Path(fname).exists():
-            input_files.append(fname)
-    
+        fname = config.base_dir / f"{category}.las"
+        if fname.exists():
+            input_files.append(str(fname))
+
     pl = []
     for f in input_files:
         pl.append(
@@ -226,7 +174,7 @@ def run_merger_pipeline(config):
     pl.append(
         {
             "type": "writers.las",
-            "filename": config.merged_filename,
+            "filename": str(config.merged_filename),
             "forward": "all",
             "extra_dims": "all",
             "minor_version": 4,
@@ -246,14 +194,12 @@ def run_merger_pipeline(config):
     for f in input_files:
         os.remove(f)
 
-
 def run_scene_chipper_pipeline(config):
-    """Run the initial chipper to create test/train/val scene splits."""
     pipeline_json = {
         "pipeline": [
             {
                 "type": "readers.las",
-                "filename": config.merged_filename,
+                "filename": str(config.merged_filename),
                 "extra_dims": EXTRA_DIMS
             },
             {
@@ -274,30 +220,19 @@ def run_scene_chipper_pipeline(config):
     print(f"Running train/test/val scene chipper with capacity={config.scene_capacity}...")
     count = pipeline.execute()
 
-    # Count number of outputs
-    file_count = 0
-    for root, dirs, files in os.walk(config.scene_dir):
-        file_count += len(files)
+    file_count = len(list(config.scene_dir.glob('*.las')))
     print(f" - chipper created {file_count} splits.")
 
-
 def run_file_chipper_pipeline(config):
-    """Run the chipper to split each scene split into manageable filesizes for Pointcept."""
-    base_dir = config.scene_dir
-    base_dir = Path(base_dir)
-    f_template = base_dir / "scene*.las"
-    scene_files = glob.glob(str(f_template))
+    scene_files = list(config.scene_dir.glob('scene*.las'))
 
     for f in scene_files:
-
-        path = Path(f)
-        template = config.split_dir + f"{path.stem}_split#.las"
+        template = str(config.split_dir / f"{f.stem}_split#.las")
         pipeline_json = {
             "pipeline": [
-                # Read the .las
                 {
                     "type": "readers.las",
-                    "filename": f,
+                    "filename": str(f),
                     "extra_dims": EXTRA_DIMS
                 },
                 {
@@ -306,7 +241,7 @@ def run_file_chipper_pipeline(config):
                 },
                 {
                     "type": "writers.las",
-                    "filename": str(template),
+                    "filename": template,
                     "forward": "all",
                     "extra_dims": "all",
                     "minor_version": 4,
@@ -315,34 +250,28 @@ def run_file_chipper_pipeline(config):
             ]
         }
         pipeline = pdal.Pipeline(json.dumps(pipeline_json))
-        print(f" - running {path.stem} chipper with capacity={file_capacity}...")
+        print(f" - running {f.stem} chipper with capacity={config.file_capacity}...")
         count = pipeline.execute()
-        file_count = 0
 
-    # Walk through all directories and files in directory_path
-    for root, dirs, files in os.walk(config.split_dir):
-        file_count += len(files)  # Add count of files in this directory
+    file_count = len(list(config.split_dir.glob('*.las')))
     print(f" - chipper created {file_count} splits across all scenes.")
 
-
 def scene_files_exist(config):
-    # Replace the # char in the pdal template with * for glob
-    template = config.scene_template.replace('#', '*')
-    scene_files = glob.glob(template)
-    return True if scene_files else False
+    scene_files = list(config.scene_dir.glob('scene*.las'))
+    return len(scene_files) > 0
 
-
-###############################################################################
-# Arg parser and main script
-###############################################################################
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Process point cloud data using PDAL.")
     parser.add_argument("dataset", type=str, help="The dataset tag to process, e.g., 'park_row'")
     parser.add_argument("resolution", type=float, help="The resolution to use in subsampling.")
     parser.add_argument(
+        "--root-dir", type=Path, default=Path(__file__).parent.parent / "data",
+        help="The root directory containing the 'raw_clouds' folder. Default is two directories up from the script location, in a 'data' folder."
+    )
+    parser.add_argument(
         "--file-capacity", type=int, default=500000,
         help="The point capacity for each file used as input to Pointcept, default is 500k."
-        )
+    )
     parser.add_argument(
         "--scene-capacity", type=int, default=2400000,
         help="The point capacity for each file train/test/val scene, default is 2.4 million."
@@ -353,10 +282,7 @@ def parse_arguments():
     )
     return parser.parse_args()
 
-
 if __name__ == '__main__':
-
-    # Retrieve args
     args = parse_arguments()
     label = args.dataset
     if label not in VALID_LABELS:
@@ -375,26 +301,31 @@ if __name__ == '__main__':
     file_capacity = args.file_capacity
     if not isinstance(file_capacity, int):
         raise ValueError(f"Provided file_capacity: {file_capacity} must be an integer.")
-    
-    # Generate a config struct to be passed as arg.
+
     MyConfig = namedtuple("MyConfig", [
         "label", "resolution", "scene_capacity", "file_capacity",
-        "base_dir", "merged_filename", "scene_dir", "scene_template", "split_dir", "split_template",
+        "root_dir", "raw_dir", "base_dir", "merged_filename", "scene_dir", "scene_template", "split_dir", "split_template",
         "pth_dir"
     ])
-    scene_dir = f"processed_clouds/resolution_{resolution}/{label}/scene_capacity_{scene_capacity}/"
+    
+    root_dir = args.root_dir.resolve()
+    raw_dir = root_dir / "raw_clouds" / label
+    base_dir = root_dir / f"processed_clouds/resolution_{resolution}/{label}"
+    scene_dir = base_dir / f"scene_capacity_{scene_capacity}"
+    
     config = MyConfig(
-        label = label, resolution=resolution, scene_capacity=scene_capacity, file_capacity=file_capacity,
-        base_dir = f"processed_clouds/resolution_{resolution}/{label}",
-        merged_filename = f"processed_clouds/resolution_{resolution}/{label}/{label}_merged.las",
-        scene_dir = scene_dir,
-        scene_template = scene_dir + "scene#.las",  # for PDAL
-        split_dir = scene_dir + f'file_capacity_{file_capacity}/',
-        split_template = scene_dir + f'file_capacity_{file_capacity}/scene*_split*.las',  # for glob
-        pth_dir = scene_dir + f'file_capacity_{file_capacity}/pth/',
+        label=label, resolution=resolution, scene_capacity=scene_capacity, file_capacity=file_capacity,
+        root_dir=root_dir,
+        raw_dir=raw_dir,
+        base_dir=base_dir,
+        merged_filename=base_dir / f"{label}_merged.las",
+        scene_dir=scene_dir,
+        scene_template=str(scene_dir / "scene#.las"),  # for PDAL
+        split_dir=scene_dir / f'file_capacity_{file_capacity}',
+        split_template=str(scene_dir / f'file_capacity_{file_capacity}/scene*_split*.las'),  # for glob
+        pth_dir=scene_dir / f'file_capacity_{file_capacity}/pth',
     )
 
-    # Print the config to the terminal with formatting for alignment
     max_field_length = max(len(field) for field in MyConfig._fields) + 1
     print("\033[1mConfiguration Details:\033[0m")
     print("-" * 30)
@@ -403,14 +334,12 @@ if __name__ == '__main__':
         print(f"\033[1m{field_name_formatted}:\033[0m \033[94m{getattr(config, field)}\033[0m")
     print("-" * 30)
 
-    # Ensure the relevant dirs
     os.makedirs(config.scene_dir, exist_ok=True)
     os.makedirs(config.pth_dir, exist_ok=True)
 
-    # If necessary, run sampling correction and merging.
-    if not Path(config.merged_filename).exists():
+    if not config.merged_filename.exists():
         print("Subsampling individual category clouds.")
-        run_all_categories(label, resolution)
+        run_all_categories(config)
         run_merger_pipeline(config)
     else:
         print(f"Merged .las output already exists for {label} at this resolution.")
@@ -418,13 +347,11 @@ if __name__ == '__main__':
         print("Merger only mode enabled, exiting now.")
         sys.exit()
 
-    # If necessary, run the scene splitter
     if not scene_files_exist(config):
         run_scene_chipper_pipeline(config)
     else:
         print("Scene files exist for this scene capacity. Skipping to file chipper...")
 
-    # Run the file splitter and .pth conversion
     run_file_chipper_pipeline(config)
     convert_all_las_to_pth(config)
 
