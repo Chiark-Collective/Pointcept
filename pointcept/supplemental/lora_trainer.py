84017468de8441c94cf85886392e06ed341be3c4
Liam Moore <liam@chiark.io>
2024-09-25 14:53:58 +0200

    implement dumping of crash trace logs (pytorch)

--- pointcept/supplemental/lora_trainer.py
+++ pointcept/supplemental/lora_trainer.py
@@ -1,3 +1,10 @@
+import time
+from datetime import datetime
+from pathlib import Path
+
+import torch
+from torch.profiler import profile, record_function, ProfilerActivity
+
 from pointcept.engines.train import Trainer
 from pointcept.models.point_prompt_training import PointPromptTrainingLoRA, configure_adamw_lora
 
@@ -7,3 +14,23 @@ class LoRATrainer(Trainer):
         if self.cfg.optimizer.pop("type") != "AdamW":
             raise NotImplementedError
         return configure_adamw_lora(self.model, **self.cfg.optimizer)
+
+    def train(self):
+        prof = profile(
+            activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+            profile_memory=True, record_shapes=True
+        ) 
+        prof.__enter__()
+        try:
+            super(LoRATrainer, self).train()
+        except torch.cuda.OutOfMemoryError as e:
+            prof.__exit__(None, None, None) ## ??
+            print(e)
+            now = datetime.utcnow().strftime("%Y%m%d_%H%M%S")
+            save_to = Path(self.cfg.save_path) / f"trace_{now}.json"
+            print(f"Exporting crash profile trace to: {save_to}")
+            prof.export_chrome_trace(str(save_to.resolve()))
+            raise 
+        else:
+            prof.__exit__(None, None, None)
+
